8fb6cf895a47aa40b649dd075bc15328dfe9b2e5
lucas huang <lucashuang2000@gmail.com>
2023-03-05 18:19:44 -0500

    Testing lucasnav

---
+++ components/navbar.js
@@ -0,0 +1,20 @@
+const nav = document.getElementsByTagName('lucasnav')[0];
+nav.innerHTML = `
+        <header>
+            <nav class="row">
+                <div id="logo-container" class="col-1">
+                    <img id="site-logo" src="/images/web-icon.png" alt="Site logo">
+                </div>
+                <div id="navbar-container" class="col-10">
+                    <ul>
+                        <li><a href="/">Works</a></li>
+                        <li><a href="/pages/about.html">About</a></li>
+                        <li><a href="/pages/LucasHuangResume.pdf" target="blank">Resume</a></li>
+                        <li><a href="https://www.linkedin.com/in/lucasjlhuang" target="blank">Linkedin</a></li>
+                    </ul>
+                </div>
+                <div id="logo-container" class="col-1">
+                </div>
+            </nav>
+        </header>
+        `
